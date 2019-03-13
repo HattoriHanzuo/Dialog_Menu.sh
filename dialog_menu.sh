fff857663e173d6a3d0c39c44aae734b23edbc15
HattoriHanzuo <svenherz96@gmail.com>
2019-03-13 10:50:04 +0100

    Hinzufügen vom nc_Projekt

--- dialog_menu.sh
+++ dialog_menu.sh
@@ -7,15 +7,15 @@ if [[ $test != *"[installiert]"* ]]
 	then sudo apt-get install dialog
 fi;
 
-	test=`apt list sl 2> /dev/null`
-	if [[ $test != *"[installiert]"* ]]
-		then 
-			passwd=`dialog --passwordbox 'Bitte gib dein -sudo- Passwort ein' 0 0
-			3>&1 1>&2 2>&3`;
-			echo $passwd | sudo apt-get install --assume-yes sl
-	fi;
-
-	sl;
+	#test=`apt list sl 2> /dev/null`
+	#if [[ $test != *"[installiert]"* ]]
+	#	then
+	#		passwd=`dialog --passwordbox 'Bitte gib dein -sudo- Passwort ein' 0 0
+	#		3>&1 1>&2 2>&3`;
+	#		echo $passwd | sudo apt-get install --assume-yes sl
+	#fi;
+    #
+	#sl;
 
 while [ true ]
 do
@@ -27,6 +27,7 @@ do
 	4 Benutzerkonten\
 	5 'Games;-)'\
 	6 Laufwerk\
+	7 NetCat\
 	0 Exit\
 	3>&1 1>&2 2>&3`;
 
@@ -64,7 +65,7 @@ do
 
 					if [ $a ];
 			        	then
-			        		passwd=`dialog --passwordbox 'Bitte das -sudo- Passwort eingeben' 0 0\
+			        		passwd=`dialog --colors --passwordbox '\Z1Bitte das -sudo- Passwort eingeben' 0 0\
 							3>&1 1>&2 2>&3`;
 			        		dialog --scrollbar --prgbox Fortschritt: "echo $passwd | sudo apt-get install --assume-yes $a" 40 100;
 						read -sn1;
@@ -83,7 +84,7 @@ do
 							3>&1 1>&2 2>&3`;
 			        		dialog --scrollbar --prgbox Fortschritt: "echo $passwd | sudo -S apt-get remove --purge --assume-yes $a" 35 100;
 			       			3>&1 1>&2 2>&3;
-							dialog --clear; 
+							dialog --clear;
 					fi; #$a
 
 				elif [ $s = '3' ]
@@ -98,9 +99,9 @@ do
 						reset
 						apt list
 						echo  -e "\033[33;7mBitte Taste drücken Um Fortzufharen ;-) ;-)\033[0m";
-						read -sn1;	
-				
-				elif [ $s = '5' ]		
+						read -sn1;
+
+				elif [ $s = '5' ]
 					then
 						suche=`dialog --inputbox 'Nach welcher APT soll gesucht werden?' 0 0\
 						3>&1 1>&2 2>&3`;
@@ -118,8 +119,8 @@ do
 							then
 								#ausgabe=`echo $passwd | sudo -S apt-get upgrade >&2`;
 								dialog --scrollbar --prgbox Fortschritt: "echo $passwd | sudo -S apt-get upgrade --assume-yes" 40 90;
-						fi;		
-				
+						fi;
+
 				elif [ $s = '7' ]
 					then
 						passwd=`dialog --passwordbox 'Bitte das -sudo- Passwort eingeben' 0 0\
@@ -128,18 +129,18 @@ do
 							then
 								#ausgabe=`echo $passwd | sudo -S apt-get upgrade >&2`;
 								dialog --scrollbar --prgbox Fortschritt: "echo $passwd | sudo -S apt-get update --assume-yes" 40 110;
-						fi;	
+						fi;
 
-						
-						
-						
+
+
+
 				fi #s1
 
 
 
 
 			fi; #s2
-		
+
 		elif [ $i = '2' ]
 			then
 				s=`dialog --menu 'Bitte auswählen!' 0 0 0\
@@ -160,14 +161,14 @@ do
 									3>&1 1>&2 2>&3`;
 									echo $passwd | sudo -S apt-get install w3m >/dev/null
 								fi;
-						        
+
 						  		if [ $s ];
 								then
 									w3m $s ;
 									read -sn1;
 								fi;	clear;
 					fi;
-					
+
 					if [ $s = '2' ]
 						then
 							s=`dialog --inputbox 'URL eingeben!!!' 0 0 "http://" \
@@ -184,7 +185,7 @@ do
 	                        3>&1 1>&2 2>&3`;
 							dialog --clear
 
-			
+
 			if [ $s ]
 			then
 
@@ -241,17 +242,17 @@ do
 							u=`dialog --inputbox 'Welcher Benutzer soll gelöscht werden?' 0 0\
 							3>&1 1>&2 2>&3`;
 							dialog --clear;
-							
+
 							if [ $u ];
 								then
 									ergebnis=`sudo deluser $u --remove-home 2>&1`;
 									dialog --infobox "$ergebnis" 0 0;
 									read -sn1;
-									
+
 							fi;
 
 					elif [ 	$s = '4' ]
-						then		
+						then
 							benutzer=`dialog --inputbox 'Das Passwort von welchem Benutzer soll geändert werden?' 0 0\
 							3>&2 2>&1 1>&3`;
 							if [ $benutzer ]
@@ -262,12 +263,12 @@ do
 										then
 											echo $passwort | sudo -S passwd $benutzer >&2;
 											read -sn1;
-											
 
 
-											
+
+
 									fi
-							fi;	
+							fi;
 
 
 					elif [ $s = '5' ]
@@ -280,22 +281,22 @@ do
 							passwd=`dialog --passwordbox 'Bitte "sudo" Passwort eingeben!' 0 0\
 							3>&2 2>&1 1>&3`;
 							if [ $benutzer ]
-								then 
+								then
 									ausgabe=`echo $passwd | sudo -S usermod -u $uid $benutzer >&2`;
 									if [ $ausgabe ]
 										then
 											dialog --prgbox 'ausgabe"$ausgabe"' 25 60
-											read -sn1; 
+											read -sn1;
 
-									fi;		
+									fi;
 							fi;
 
 
 
-					
+
 
 				fi;
-			fi;	
+			fi;
 
 		elif [ $i = '5' ]
 			then
@@ -307,105 +308,105 @@ do
 						5 'Greed'\
 						6 'View the Star Wars Strory'\
 						3>&1 1>&2 2>&3`;
-						dialog --clear;	
+						dialog --clear;
 
 						if [ $s ]
 							then
 						if [ $s = '1' ]
 							then
 								test=`apt list ninvaders 2> /dev/null`
-								
+
 								if [[ $test != *"[installiert]"* ]]
 									then
 										passwd=`dialog --passwordbox 'Bitte sudo Passwort eingaben' 0 0\
 										3>&2 2>&1 1>&3`;
 										echo $passwd | sudo -S apt-get install --assume-yes ninvaders >/dev/null
-										
-								fi	
-								
+
+								fi
+
 								ninvaders;
-														
+
 						fi;
-							
+
 
 							if [ $s = '2' ]
 							then
 								test=`apt list sudoku 2> /dev/null`
-								
+
 								if [[ $test != *"[installiert]"* ]]
 									then
 										passwd=`dialog --passwordbox 'Bitte sudo Passwort eingaben' 0 0\
 										3>&2 2>&1 1>&3`;
 										echo $passwd | sudo -S apt-get install --assume-yes sudoku >/dev/null
-										
-								fi	
+
+								fi
 								beep;
 								sudoku;
-														
+
 							fi;
 
 							if [ $s = '3' ]
 							then
 								test=`apt list bastet 2> /dev/null`
-								
+
 								if [[ $test != *"[installiert]"* ]]
 									then
 										passwd=`dialog --passwordbox 'Bitte sudo Passwort eingaben' 0 0\
 										3>&2 2>&1 1>&3`;
 										echo $passwd | sudo -S apt-get install --assume-yes bastet >/dev/null
-										
-								fi	
-								
-								xterm -e 'beep -f 330 -l 150 -n -f 1 -l 40 -n -f 494 -l 159 -n -f 1 -l 40 -n -f 660 -l 150 -n -f 1 -l 40 -n -f 590 -l 150 -n -f 660 -l 150 -n -f 494 -l 100 -n -f 494 -l 100 -n -f 523 -l 150 -n -f 1 -l 40 -n -f 440 -l 150 -n -f 1 -l 40 -n -f 494 -l 150 -n -f 1 -l 40 -n -f 392 -l 100 -n -f 392 -l 100 -n -f 440 -l 150 -n -f 370 -l 150 -n -f 1 -l 40 -n -f 392 -l 150 -n -f 1 -l 40 -n -f 330 -l 100 -n -f 330 -l 100 -n -f 370 -l 150 -n -f 1 -l 40 -n -f 294 -l 150 -n -f 1 -l 40 -n -f 330 -l 150 -n -f 247 -l 100 -n -f 247 -l 100 -n -f 261 -l 150 -n -f 1 -l 40 -n -f 311 -l 150 -n -f 1 -l 40 -n -f 330 -l 150 -n -f 1 -l 40 -n -f 247 -l 100 -n -f 247 -l 100 -n -f 262 -l 150 -n -f 1 -l 40 -n -f 370 -l 150 -n -f 1 -l 40 -n -f 330 -l 150 -n -f 1 -l 40 -n -f 494 -l 159 -n -f 1 -l 40 -n -f 660 -l 150 -n -f 1 -l 40 -n -f 590 -l 150 -n -f 660 -l 150 -n -f 494 -l 100 -n -f 494 -l 100 -n -f 523 -l 150 -n -f 1 -l 40 -n -f 440 -l 150 -n -f 1 -l 40 -n -f 494 -l 150 -n -f 1 -l 40 -n -f 392 -l 100 -n -f 392 -l 100 -n -f 440 -l 150 -n -f 370 -l 150 -n -f 1 -l 40 -n -f 392 -l 150 -n -f 1 -l 40 -n -f 330 -l 100 -n -f 330 -l 100 -n -f 370 -l 150 -n -f 1 -l 40 -n -f 294 -l 150 -n -f 1 -l 40 -n -f 330 -l 150 -n -f 247 -l 100 -n -f 247 -l 100 -n -f 261 -l 150 -n -f 1 -l 40 -n -f 311 -l 150 -n -f 1 -l 40 -n -f 330 -l 150 -n -f 1 -l 40 -n -f 247 -l 100 -n -f 247 -l 100 -n -f 262 -l 150 -n -f 1 -l 40 -n -f 370 -l 150 -n -f 1 -l 40 -n -f 330 -l 150 -n -f 1 -l 40' 
-								bastet; 
 
-														
+								fi
+
+								xterm -e 'beep -f 330 -l 150 -n -f 1 -l 40 -n -f 494 -l 159 -n -f 1 -l 40 -n -f 660 -l 150 -n -f 1 -l 40 -n -f 590 -l 150 -n -f 660 -l 150 -n -f 494 -l 100 -n -f 494 -l 100 -n -f 523 -l 150 -n -f 1 -l 40 -n -f 440 -l 150 -n -f 1 -l 40 -n -f 494 -l 150 -n -f 1 -l 40 -n -f 392 -l 100 -n -f 392 -l 100 -n -f 440 -l 150 -n -f 370 -l 150 -n -f 1 -l 40 -n -f 392 -l 150 -n -f 1 -l 40 -n -f 330 -l 100 -n -f 330 -l 100 -n -f 370 -l 150 -n -f 1 -l 40 -n -f 294 -l 150 -n -f 1 -l 40 -n -f 330 -l 150 -n -f 247 -l 100 -n -f 247 -l 100 -n -f 261 -l 150 -n -f 1 -l 40 -n -f 311 -l 150 -n -f 1 -l 40 -n -f 330 -l 150 -n -f 1 -l 40 -n -f 247 -l 100 -n -f 247 -l 100 -n -f 262 -l 150 -n -f 1 -l 40 -n -f 370 -l 150 -n -f 1 -l 40 -n -f 330 -l 150 -n -f 1 -l 40 -n -f 494 -l 159 -n -f 1 -l 40 -n -f 660 -l 150 -n -f 1 -l 40 -n -f 590 -l 150 -n -f 660 -l 150 -n -f 494 -l 100 -n -f 494 -l 100 -n -f 523 -l 150 -n -f 1 -l 40 -n -f 440 -l 150 -n -f 1 -l 40 -n -f 494 -l 150 -n -f 1 -l 40 -n -f 392 -l 100 -n -f 392 -l 100 -n -f 440 -l 150 -n -f 370 -l 150 -n -f 1 -l 40 -n -f 392 -l 150 -n -f 1 -l 40 -n -f 330 -l 100 -n -f 330 -l 100 -n -f 370 -l 150 -n -f 1 -l 40 -n -f 294 -l 150 -n -f 1 -l 40 -n -f 330 -l 150 -n -f 247 -l 100 -n -f 247 -l 100 -n -f 261 -l 150 -n -f 1 -l 40 -n -f 311 -l 150 -n -f 1 -l 40 -n -f 330 -l 150 -n -f 1 -l 40 -n -f 247 -l 100 -n -f 247 -l 100 -n -f 262 -l 150 -n -f 1 -l 40 -n -f 370 -l 150 -n -f 1 -l 40 -n -f 330 -l 150 -n -f 1 -l 40'
+								bastet;
+
+
 							fi;
 
 							if [ $s = '4' ]
 							then
 								test=`apt list nsnake 2> /dev/null`
-								
+
 								if [[ $test != *"[installiert]"* ]]
 									then
 										passwd=`dialog --passwordbox 'Bitte sudo Passwort eingaben' 0 0\
 										3>&2 2>&1 1>&3`;
 										echo $passwd | sudo -S apt-get install --assume-yes nsnake >/dev/null
-										
-								fi	
-								
+
+								fi
+
 								nsnake;
-														
+
 							fi;
 
 							if [ $s = '5' ]
 							then
 								test=`apt list greed 2> /dev/null`
-								
+
 								if [[ $test != *"[installiert]"* ]]
 									then
 										passwd=`dialog --passwordbox 'Greed ist noch nicht installiert.\
 										Gib bitte das -sudo- Passwort ein damit die Installation beginnen kann ;-) ' 0 0\
 										3>&2 2>&1 1>&3`;
 										echo $passwd | sudo -S apt-get install --assume-yes greed >/dev/null
-										
-								fi	
-								
+
+								fi
+
 								greed;
-														
+
 							fi;
 
 							if [ $s = '6' ]
 								then
 									beep -l 60000 -f 1 -D 100 --new -l 350 -f 392 -D 100 --new -l 350 -f 392 -D 100 --new -l 350 -f 392 -D 100 --new -l 250 -f 311.1 -D 100 --new -l 25 -f 466.2 -D 100 --new -l 350 -f 392 -D 100 --new -l 250 -f 311.1 -D 100 --new -l 25 -f 466.2 -D 100 --new -l 700 -f 392 -D 100 --new -l 350 -f 587.32 -D 100 --new -l 350 -f 587.32 -D 100 --new -l 350 -f 587.32 -D 100 --new -l 250 -f 622.26 -D 100 --new -l 25 -f 466.2 -D 100 --new -l 350 -f 369.99 -D 100 --new -l 250 -f 311.1 -D 100 --new -l 25 -f 466.2 -D 100 --new -l 700 -f 392 -D 100 --new -l 350 -f 784 -D 100 --new -l 250 -f 392 -D 100 --new -l 25 -f 392 -D 100 --new -l 350 -f 784 -D 100 --new -l 250 -f 739.98 -D 100 --new -l 25 -f 698.46 -D 100 --new -l 25 -f 659.26 -D 100 --new -l 25 -f 622.26 -D 100 --new -l 50 -f 659.26 -D 400 --new -l 25 -f 415.3 -D 200 --new -l 350 -f 554.36 -D 100 --new -l 250 -f 523.25 -D 100 --new -l 25 -f 493.88 -D 100 --new -l 25 -f 466.16 -D 100 --new -l 25 -f 440 -D 100 --new -l 50 -f 466.16 -D 400 --new -l 25 -f 311.13 -D 200 --new -l 350 -f 369.99 -D 100 --new -l 250 -f 311.13 -D 100 --new -l 25 -f 392 -D 100 --new -l 350 -f 466.16 -D 100 --new -l 250 -f 392 -D 100 --new -l 25 -f 466.16 -D 100 --new -l 700 -f 587.32 -D 100 --new -l 350 -f 784 -D 100 --new -l 250 -f 392 -D 100 --new -l 25 -f 392 -D 100 --new -l 350 -f 784 -D 100 --new -l 250 -f 739.98 -D 100 --new -l 25 -f 698.46 -D 100 --new -l 25 -f 659.26 -D 100 --new -l 25 -f 622.26 -D 100 --new -l 50 -f 659.26 -D 400 --new -l 25 -f 415.3 -D 200 --new -l 350 -f 554.36 -D 100 --new -l 250 -f 523.25 -D 100 --new -l 25 -f 493.88 -D 100 --new -l 25 -f 466.16 -D 100 --new -l 25 -f 440 -D 100 --new -l 50 -f 466.16 -D 400 --new -l 25 -f 311.13 -D 200 --new -l 350 -f 392 -D 100 --new -l 250 -f 311.13 -D 100 --new -l 25 -f 466.16 -D 100 --new -l 300 -f 392.00 -D 150 --new -l 250 -f 311.13 -D 100 --new -l 25 -f 466.16 -D 100 --new -l 700 -f 392 &
-									
+
 									dialog --prgbox "telnet towel.blinkenlights.nl" 15 80
-									
-							fi;		
+
+							fi;
 
 						fi; #$s
 
-	
+
 		elif [ $i = '6' ]
 		then
 			s=`dialog --menu 'Bitte wähle eine Aktion!' 0 0 0\
@@ -414,7 +415,7 @@ do
 						3 'Laufwerk mit Nullen überschreiben'\
 						4 'SMART-DATA Check'\
 						3>&1 1>&2 2>&3`;
-			dialog --clear;	
+			dialog --clear;
 
 			if [ $s = '1' ]
 				then
@@ -422,7 +423,7 @@ do
 					dialog --infobox "$sd" 0 0;
 					read -sn1;
 
-				elif [ $s = 2 ];	
+				elif [ $s = 2 ];
 					then
 						passwd=`dialog --passwordbox 'Sudo Passwort eingeben' 0 0\
 						3>&1 1>&2 2>&3`;
@@ -436,13 +437,13 @@ do
 										3>&1 1>&2 2>&3`;
 										if [ $fomat ]
 											then
-												echo $passwd | sudo -S mount /dev/$laufwerk; 
+												echo $passwd | sudo -S mount /dev/$laufwerk;
 												dialog --prgbox "echo $passwd | sudo -S mkfs.$fomat /dev/$laufwerk" 15 80;
 										fi;
-								fi;				
-						fi;					
+								fi;
+						fi;
 
-					
+
 				elif [ $s = '3' ]
 					then
 						passwd=`dialog --passwordbox 'Sudo Passwort eingeben' 0 0\
@@ -456,17 +457,17 @@ do
 										how=`dialog --inputbox '0= Mit Nullen\
 										1= Zufallszahlen' 0 40\
 										3>&1 1>&2 2>&3`;
-										
+
 										if [ $how ]
 											then
 												#dialog --clear;
 												dialog --scrollbar --prgbox Fortschritt: "sudo shred -vzn $how $shred" 20 60;
 										fi;		read -sn1;
 
-								fi;	
-						fi;		
+								fi;
+						fi;
 
-				
+
 				elif [ $s = '4' ]
 					then
 						passwd=`dialog --passwordbox 'Sudo Passwort eingeben' 0 0\
@@ -478,28 +479,73 @@ do
 								if [ $smart ]
 									then
 										dialog --prgbox SMART-DATA " echo $passwd | sudo -S smartctl -a $smart" 100 120;
-								fi
-						fi
+								fi;
+						fi;
 
 
-					
 
-						
-
-
-								
 
 			fi;
 
 
+        elif [ $i = '7' ]
+            then
+            s=`dialog --menu 'NetCat Menü' 0 0 0\
+            1 'Auf $port lauschen für Text'\
+            2 'Auf $port lauschen zum Datein empfangen in $pfad'\
+            3 'Verbindung zu $ip auf $port aufbauen für Text'\
+            4 'Verbindung zu $ip auf $port aufbauen für Datein'\
+            3>&2 2>&1 1>&3;`
+            dialog --clear;
+
+            if [ $s ]
+                then
+
+                    if [ $s = '1' ]
+                        then
+
+                            port=`dialog --inputbox 'Bitte Port zum lauschen eingeben' 0 70 \
+                            3>&2 2>&1 1>&3`;
+
+                            if [ $port ];
+                                then
+                                    dialog --scrollbar --prgbox test "nc -l $port" 30 60;
+
+
+                            fi;
+
+                    elif [ $s = '2' ]
+                        then
+                            port=`dialog --colors --inputbox 'Bitte \Zb\Z1Port\Z0\ZB zum lauschen eingeben' 0 70 \
+                            3>&2 2>&1 1>&3`;
+                            pfad=`dialog --dselect /home/$USER/ 10 70 \
+                            3>&2 2>&1 1>&3`;
+
+                            if [ $pfad ] && [ $port ];
+                                then
+                                    dialog --scrollbar --prgbox test "nc -l $port > '$pfad'" 60 90;
+
+                            fi;
+
+
+
+
+                    fi;
+
+
+
+
+            fi;
+
+
 		fi; #i1
-	
+
 	else
 		clear; exit;
-	
+
 
 	fi; #i2
-done
+done;
 
 
 #written by Sven Herz	©
